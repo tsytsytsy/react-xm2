c8674c769add65f057e6e0b5a2e61a4c6a4c9e9b
ranqingju <183527800@qq.com>
2018-12-21 21:09:25 +0800

    ran

--- time/src/components/buy/buy.js
+++ time/src/components/buy/buy.js
@@ -1,13 +1,16 @@
 import React,{Component} from "react";
 import Header from "../../common/js/header.js"
-
-
+import Seek from "./children/seek.js"
+import Address from "./children/address.js"
+import "./style/css/seek.css";
+import "./style/css/address.css";
 export default class Buy extends Component{
 	render(){
 		return (
 			<div>
 				<Header/>
-				我是购票
+				 <Seek/>
+				 <Address/>
 			</div>
 		)
 	}
---
+++ time/src/components/buy/children/address.js
@@ -0,0 +1,25 @@
+import React,{Component} from "react"
+
+export default class Address extends Component{
+	render(){
+		return(
+			<div id="address">
+					<a href="">
+					<span>离我最近</span>
+					<i className="fa fa-angle-down"></i>
+					</a>
+					<a href="">
+					<span>全城</span>
+					<i className="fa fa-angle-down"></i>
+					</a>
+					<a href="">
+					<span>歌厅特效</span>
+					<i className="fa fa-angle-down"></i>
+					</a>
+			</div>			
+					
+					
+			
+		)
+	}	
+}	
