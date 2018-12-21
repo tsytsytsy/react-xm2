01e76a0d39fb30d6eece6c4333c5199db69eb51e
tsytsytsy <18242055523@163.com>
2018-12-21 09:57:50 +0800

    解除bug

--- time/src/action/actionCreator.js
+++ time/src/action/actionCreator.js
@@ -1,5 +1,19 @@
 import {fetch} from "whatwg-fetch";
 
+//https://api-m.mtime.cn/PageSubArea/HotPlayMovies.api?locationId=290
+export const movie_data = () =>({
+    type : "MOVIE_DATA",
+    payload : new Promise(resolve=>{
+        let url = "/PageSubArea/HotPlayMovies.api?locationId=290"
+        fetch(url)
+        .then(res=>res.json())
+        .then((data)=>{
+            resolve(data)
+        })
+    })
+})
+
+
 export const getData_action = () =>({
     type : "BANNERPHONT",
     payload : new Promise(resolve=>{
--- time/src/setupProxy.js
+++ time/src/setupProxy.js
@@ -2,13 +2,11 @@ const proxy = require("http-proxy-middleware");
 
 
 module.exports = (app)=>{
-<<<<<<< HEAD
 	app.use("/PageSubArea",proxy({
 		target:"https://api-m.mtime.cn",
-=======
+/*		
 	app.use("/Service",proxy({
-		target:"https://m.mtime.cn",
->>>>>>> yang
+		target:"https://m.mtime.cn",*/
 		changeOrigin:true
 	}))
 }
